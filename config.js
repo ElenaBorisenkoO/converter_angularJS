8e49ccfeadeea86f637e957ce17817845f0c1731
Lena <elena.borisenko1991@gmail.com>
2019-03-29 10:02:02 +0000

    add file config

---
+++ config.js
@@ -0,0 +1,12 @@
+const projectConfigVars = {
+    apiKey: "AIzaSyBn1zDplUrVMJnXmmc2qQDmn3JcKhrjkd4",
+    authDomain: "currencyconverter-bbce9.firebaseapp.com",
+    databaseURL: "https://currencyconverter-bbce9.firebaseio.com",
+    projectId: "currencyconverter-bbce9",
+    storageBucket: "currencyconverter-bbce9.appspot.com",
+    messagingSenderId: "885797039896",
+    GOOGLE_PLACES_API_KEY: "AIzaSyDSTtBBFiqGY_Wx5q3cPIUfRpO6pZTj77I",
+    weatherAPIkey: "b0f2a7928e6c418baf01f03c9101c5df"
+}
+
+module.exports = projectConfigVars
